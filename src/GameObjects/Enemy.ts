a780a56f95dcdccb283a7db3a94843dc892c9cae
Sachin Sharma <sachin.sharma1@maersk.com>
2023-06-01 23:16:22 +0530

    Add rough draft of enemy

---
+++ src/GameObjects/Enemy.ts
@@ -0,0 +1,12 @@
+import {Scene} from 'phaser'
+export default class Enemy extends Phaser.Physics.Matter.Sprite
+{
+    world:Phaser.Physics.Matter.World;
+    constructor(scene:Scene,x:number,y:number,texture: string | Phaser.Textures.Texture)
+    {
+        super(scene.matter.world,x,y,texture);
+        this.world=scene.matter.world;
+        
+    }
+    
+}
