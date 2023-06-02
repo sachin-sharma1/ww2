e33b6580ee79c1101f926c1ae2bffec371837d55
Sachin Sharma <sachin.sharma1@maersk.com>
2023-06-02 23:07:57 +0530

    Add tween

--- src/GameObjects/Enemy.ts
+++ src/GameObjects/Enemy.ts
@@ -6,13 +6,24 @@ export default class Enemy extends Phaser.Physics.Matter.Sprite
     {
         super(scene.matter.world,x,y,texture);
         this.world=scene.matter.world;
-        this.on(Phaser.GameObjects.Events.DESTROY,this.onDelete);
+      //  this.on(Phaser.GameObjects.Events.DESTROY,this.onDelete);
+        const onUpdate = (tween:Phaser.Tweens.Tween)=>{
+            const value = Math.floor(tween.getValue());
+            this.setTint(Phaser.Display.Color.GetColor(value, value, value));
+        }
+        
+        this.scene.tweens.addCounter({
+            from: 0,
+            to: 255,
+            duration: 3000,
+            onUpdate
+            
+        });
         
     }
     
     onDelete()
     {
-       
         console.log("i am being deleted")
     }
     
